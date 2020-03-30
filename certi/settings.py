860f74b428c43a67d2feeb899e8aa4a335dbc7e3
unknown <sobaro2013@gmail.com>
2020-03-30 20:27:57 +0300

    ok

--- certi/settings.py
+++ certi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "r$rkxnyg(jmz5+q9z8jh!pu%h4f0mnzu&i-)#gxnlikb2r+_(3"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["nshama.herokuapp.com",]
+ALLOWED_HOSTS = ["nshama.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
