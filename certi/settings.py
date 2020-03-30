59eac35c1ece80800a64da84b4b788af94b8305e
unknown <sobaro2013@gmail.com>
2020-03-30 18:40:42 +0300

    Updated settings.

--- certi/settings.py
+++ certi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "r$rkxnyg(jmz5+q9z8jh!pu%h4f0mnzu&i-)#gxnlikb2r+_(3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["nshama.herokuapp.com",]
 
 
 # Application definition
