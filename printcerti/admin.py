7bb4096e588ff1bfcaa9f73a83ba6e81ba10fc83
unknown <sobaro2013@gmail.com>
2020-04-01 22:44:25 +0300

    Added searching.

--- printcerti/admin.py
+++ printcerti/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Person, Certificate
+from .models import Person
 
 
-admin.site.register(Person)
-admin.site.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ['w_name']
+    search_fields = ["w_name",]
+
+
+admin.site.register(Person, admin_class=CertificateAdmin)
+
+
